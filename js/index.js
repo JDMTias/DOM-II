a0e2d4e4d287c8acae42e76611a29b38a00239c1
JDMTias <matiasiturbide@gmail.com>
2020-05-05 22:46:44 -0600

    I know I need to commit more 9/10 done

--- js/index.js
+++ js/index.js
@@ -21,6 +21,7 @@ const cantLeave = getSem('.nope');
 function onEscape(event) {
     if (event.key === 'Escape'){
         cantLeave.classList.remove('nope')
+        console.log('ah ah ah you didnt say they magic words')
     }
 }
 
@@ -29,16 +30,86 @@ window.addEventListener('keydown', onEscape)
 
 // wheel
 
+const bigAdv = getSem('.footer p');
 
-// drag/drop
-
-function dragStartHandler(event){
-    event.dataTransfer.setData('img/jpeg', event.target.class)
-    event.dataTransfer.dropEffect = 'move';
+function fontZoom(event) {
+    event.target.style.fontSize = '25px';
 }
 
-window.addEventListener('DomContentLoaded',() => {
-    const bus = getSem('.buspic')
-    bus.addEventListener('dragstart', dragStartHandler);
-})
+bigAdv.addEventListener('wheel', fontZoom);
 
+// drag/drop-have to finish still don't get it
+
+// function dragStartHandler(event){
+//     event.dataTransfer.setData('img/jpeg', event.target.class)
+//     event.dataTransfer.dropEffect = 'move';
+// }
+
+// window.addEventListener('DomContentLoaded',() => {
+//     const bus = getSem('.buspic')
+//     bus.addEventListener('dragstart', dragStartHandler);
+
+
+// load 
+
+window.addEventListener('load', (event) => {
+    console.log('say the magic word to escape');
+});
+
+// focus
+
+const navBco = getSem('.nav a');
+
+function blueBackg (event) {
+    event.target.style.background = 'lightblue'
+}
+
+function clearBackg (event) {
+    event.target.style.background = '';
+}
+
+navBco.addEventListener('focus', blueBackg);
+  
+navBco.addEventListener('blur', clearBackg);
+
+// resize
+
+function windowSize (event) {
+    console.log('windowheight:', window.innerHeight);
+    console.log('windowwidth', window.innerWidth)
+}
+
+window.addEventListener('resize', windowSize);
+
+// scroll 
+
+const funTitle = getSem('.logo-heading');
+
+function rotateLogo (event) {
+    funTitle.style.transform = 'rotate('+window.pageYOffset+'deg)';
+}
+
+window.addEventListener('scroll', rotateLogo )
+
+// select 
+
+const trapped = getSem('#warning')
+
+function selectAlert (event){
+    trapped.select();
+    alert('You will be kidnapped');
+}
+
+trapped.addEventListener('select', selectAlert)
+
+// DoubleClick
+
+const bigTitle = getSem('.intro h2');
+
+function change (event) {
+    event.target.style.color = 'red';
+}
+
+bigTitle.addEventListener('dblclick', change)
+
+  
